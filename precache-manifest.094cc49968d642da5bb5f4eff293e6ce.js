5eb913c2bf42710e70fb29b88eda88c48eeaf475
qiwenjing <qiwenjing@kuaishou.com>
2020-08-20 11:03:26 +0800

    Updates

---
+++ precache-manifest.094cc49968d642da5bb5f4eff293e6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bcfbdcbbe605ccb68e413a1a51be8d",
+    "url": "/index/index.html"
+  },
+  {
+    "revision": "8a43646790257c4065ee",
+    "url": "/index/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ba6f3c90b8f2d8669308",
+    "url": "/index/static/js/2.4ea20644.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/index/static/js/2.4ea20644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a43646790257c4065ee",
+    "url": "/index/static/js/main.3db2ebbe.chunk.js"
+  },
+  {
+    "revision": "ec085fb3ca73da353909",
+    "url": "/index/static/js/runtime-main.acea574d.js"
+  }
+]);
